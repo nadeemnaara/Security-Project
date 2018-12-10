46b73b74958587e7423bc4e755d71e31d70fa802
nadeemnaara <nadeemn.94@hotmail.com>
2018-12-10 19:17:24 +0200

    added funtions to tools

--- tools.py
+++ tools.py
@@ -1,5 +1,4 @@
 
-
 def try_catch_wrapper(func_ptr, *args):
     """
     calls the function within try-catch scope.
@@ -48,3 +47,60 @@ def list_to_str(slist, sep):
         ret_str += s + sep
 
     return ret_str
+
+# ------------------------------------------------------------------------
+
+
+def assert_and_raise(condition, err_msg):
+    """
+    asserts the given condition and raises an exception in case of failure with the given
+    error message.
+    :param condition: bolean.
+    :param err_msg: an error message in case of failure.
+    """
+    if not condition:
+        raise Exception(err_msg)
+
+# ------------------------------------------------------------------------
+
+
+class Log:
+
+    # print tools.
+
+    @staticmethod
+    def stage(to_print):
+        """
+        prints in the following format:
+        ========================================================
+        # the message to print
+        ========================================================
+        :param to_print: the string to print.
+        """
+        to_print_len = len(to_print)
+        line_format = ''
+        for i in range(to_print_len + 5):
+            line_format += '='
+        print(line_format)
+        print('## {}'.format(to_print))
+        print(line_format)
+
+    # ------------------------------------------------------------------------
+
+    # TODO: code duplication
+    @staticmethod
+    def info(to_print):
+        """
+        prints in the following format:
+        ----------------------------------------------------------
+        --INFO-- the message to print
+        ----------------------------------------------------------
+        :param to_print: the string to print.
+        """
+        to_print_len = len(to_print)
+        line_format = ''
+        for i in range(to_print_len + 5):
+            line_format += '-'
+        print(line_format)
+        print('--INFO-- {}'.format(to_print))
+        print(line_format)
